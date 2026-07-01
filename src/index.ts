c759a42dd07b831bfe122c601d8bf404fec35e7e
Yosia Sipahutar <yosiasipahutar24@gmail.com>
2026-07-01 18:05:49 +0700

    init: initial commit for typescript for RESTAPI

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express, {
+  type Application,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+
+const PORT: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Server TypeScript Express Berhasil Berjalan!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on  http://localhost:${PORT}`);
+});
